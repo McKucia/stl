6b58d9ef37b26bea9faf312637019c66154f0723
McKucia <macikucia@onet.pl>
2020-06-14 18:09:28 +0200

    Adding more untracked files

---
+++ module2/homework/transformContainers/transform.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <deque>
+#include <string>
+#include <list>
+#include <map>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>, std::deque<int>);
