770e4d92499c7bc2b779256667e6f7945085b478
McKucia <macikucia@onet.pl>
2020-06-18 00:49:31 +0200

    GrayScaleImages implementation

--- module2/homework/grayscaleImages/compression.cpp
+++ module2/homework/grayscaleImages/compression.cpp
@@ -1,12 +1,14 @@
 #include "compression.hpp"
 
 #include <iostream>
+#include <algorithm>
 
-void printMap(const std::array<std::array<uint8_t, width>, height>& bitmap) {
+void printMap(const bitmapArr& bitmap) {
+    
 
     for (const auto& arr : bitmap) {
-        for (auto& el : arr) {
-            if (el <= ASCIIno32)
+        for (auto el : arr) {
+            if (iscntrl(el))
                 std::cout << " ";
             else
                 std::cout << el;
@@ -15,47 +17,31 @@ void printMap(const std::array<std::array<uint8_t, width>, height>& bitmap) {
     }
 }
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& grayScale) {
-    std::vector<std::pair<uint8_t, uint8_t>> compressed;
-    compressed.reserve(width * height);
-    int count = 0;
-    int tmp = 0;
+grayScaleVec compressGrayscale(const bitmapArr& grayScale) {
 
-    for(const auto& row : grayScale){
-        auto _prev = row.at(0);
-        for(const auto& column : row){
-            if(_prev == column) {
-                count++;
-                _prev = column;
-                continue;
-            }
-            compressed.push_back(std::make_pair(_prev, count));
-            _prev = column;
-            count = 1;
+    grayScaleVec compressed;
+    compressed.reserve(width * height);
+
+    for (const auto& row : grayScale) {
+        for(auto it = row.begin(); it != row.end();){
+            auto firstNotMatch = std::find_if(it, row.end(), [&it](const auto &pixel) { return pixel != *it; });
+            compressed.push_back({ *it, std::distance(it, firstNotMatch) });
+            it = firstNotMatch;
         }
-        compressed.push_back(std::make_pair(_prev, count));
-        count = 0;
     }
 
     compressed.shrink_to_fit();
-
     return compressed;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressed ){
-    std::array<std::array<uint8_t, width>, height> decompressed;
-    std::array<uint8_t , width> tmpArray;
-    int i = 0, j = 0;
+bitmapArr decompressGrayscale(const grayScaleVec& compressed) {
 
-    for(const auto& pair : compressed){
-        for(int count = 0; count < pair.second; count++){
-            tmpArray[i++] = pair.first;
-        }
-        if(i == width){
-            decompressed[j++] = tmpArray;
-            i = 0;
-        }
-    }
+    bitmapArr  decompressed;
+    auto it = decompressed.front().begin();
+
+    std::for_each(compressed.begin(), compressed.end(), [&it](const auto& pair){
+        it = std::fill_n(it, pair.second, pair.first);
+    });
 
     return decompressed;
 }
--- module2/homework/grayscaleImages/compression.hpp
+++ module2/homework/grayscaleImages/compression.hpp
@@ -3,9 +3,11 @@
 #include <array>
 #include <vector>
 
-constexpr char ASCIIno32 = ' ';
 constexpr size_t width = 32;
 constexpr size_t height = 32;
+
+using bitmapArr = std::array<std::array<uint8_t, width>, height>;
+using grayScaleVec = std::vector<std::pair<uint8_t, uint8_t>>;
 
 void printMap(const std::array<std::array<uint8_t, width>, height>& bitmap);
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>&);
